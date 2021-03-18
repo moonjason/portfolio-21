114e030a0c9c83b3088d36048270f54f248a3ffc
moonjason <jason@activetheory.net>
2021-03-17 17:22:46 -0700

    components, menu structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Switch } from 'react-router-dom';
+//components
+// import Header from './components/Header/Header';
+// import Nav from './components/Nav/Nav';
+// import Home from './components/Home/Home';
+import Menu from './components/Menu/Menu';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-
+      <Switch>
+        {/* <Route/ > */}
+      </Switch>
+      <Menu/>
     </div>
   );
 }
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,21 @@
+import './menu.scss'
+
+function Menu() {
+
+    return (
+      <div class="menu-container">
+          <div class="menu">
+            <div class="menu-row"> 
+              <div class="menu-left">
+                Hi
+              </div>
+              <div class="menu-right">
+                Hi
+              </div>
+            </div>
+          </div>
+      </div>
+    );
+  }
+
+export default Menu
