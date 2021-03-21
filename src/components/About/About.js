355d99f3acb52ff1af903821a1d880e0b6578a85
moonjason <jasonmoonjm@gmail.com>
2021-03-21 10:13:43 -0700

    about structure set up

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,41 @@
+import './About.scss'
+
 function About() {
 
     return (
-      <div>
-          hello
-      </div>
+      <div className="about-container">
+          <div className="about"> 
+            <div className="column">
+              <div className="back__btn">
+                <img src="https://via.placeholder.com/25" alt="placeholder"/>
+              </div>
+            </div>
+            <div className="column">
+              <div className="photo">
+                <img src="https://via.placeholder.com/185" alt="placeholder"/>
+              </div>
+            </div>
+            <div className="column">
+              <div className="text">
+                text
+                text
+                text
+                text
+                text
+                text
+                text
+                text
+                text
+                text
+                text
+                text
+                text
+                text
+              </div>
+            </div>
+          </div>
+          <div className="continue">continue</div>
+        </div>
     );
   }
 
