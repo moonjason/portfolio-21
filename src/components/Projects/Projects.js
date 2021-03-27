a32fd0c6eb2bf52e344889463211c0e84c2fbfda
moonjason <jasonmoonjm@gmail.com>
2021-03-26 20:13:28 -0700

    base accordion set up on projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,7 @@ import { projects } from './content.json';
 function Projects() {
 
     const setContent = () => { 
-      
+
     }
 
     return (
@@ -15,35 +15,50 @@ function Projects() {
                 <input type="radio" id="rd1" name="rd"/>
                 <label className="tab-label" for="rd1">E.C.H.O.</label>
                 <div className="tab-content">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, facilis.
+                  <img src="https://via.placeholder.com/150" alt="project"/>
+                  <div className="tab-text">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, facilis.
+                  </div>
                 </div>
               </div>
               <div className="tab">
                 <input type="radio" id="rd2" name="rd"/>
                 <label className="tab-label" for="rd2">Spotify Wrapped 2020</label>
                 <div className="tab-content">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, aut.
+                  <img src="https://via.placeholder.com/150" alt="project"/>
+                  <div className="tab-text">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, facilis.
+                  </div>
                 </div>
               </div>
               <div className="tab">
                 <input type="radio" id="rd3" name="rd"/>
                 <label className="tab-label" for="rd3">Promo Cards by Spotify</label>
                 <div className="tab-content">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, aut.
+                  <img src="https://via.placeholder.com/150" alt="project"/>
+                  <div className="tab-text">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, facilis.
+                  </div>
                 </div>
               </div>
               <div className="tab">
                 <input type="radio" id="rd4" name="rd"/>
                 <label className="tab-label" for="rd4">Sign for Jimmie</label>
                 <div className="tab-content">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, aut.
+                  <img src="https://via.placeholder.com/150" alt="project"/>
+                  <div className="tab-text">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, facilis.
+                  </div>
                 </div>
               </div>
               <div className="tab">
-                <input type="radio" id="rd4" name="rd"/>
-                <label className="tab-label" for="rd4">Trolli Delicioulsy Dark Escape</label>
+                <input type="radio" id="rd5" name="rd"/>
+                <label className="tab-label" for="rd5">Trolli Deliciously Dark Escape</label>
                 <div className="tab-content">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, aut.
+                  <img src="https://via.placeholder.com/150" alt="project"/>
+                  <div className="tab-text">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, facilis.
+                  </div>
                 </div>
               </div>
           </div>
