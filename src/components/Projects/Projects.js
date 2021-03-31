bb58ac6b0e42ff07e550cc1a99092bbf2875ee22
moonjason <jasonmoonjm@gmail.com>
2021-03-31 01:16:07 -0700

    fix content loading with useEffect, update hosting settings

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,28 @@
 import './projects.scss'
-import { projects, personal } from './content.json';
+// import { projects, personal } from './data/content.json';
+import { useEffect, useState } from 'react';
 
 function Projects() {
+    const [data, setData] = useState(null)
+
+    useEffect(()=> {
+      async function fetchData() {
+
+        await fetch("data/content.json")
+          .then(response => response.json())
+          .then(responseJson => {
+            setData(responseJson)
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+      fetchData();
+    }, [])
 
     const setContent = () => { 
       let delay = 0.1
-      return  projects.map( (project, i) => {
+      return  data.projects.map( (project, i) => {
         let style = { 
           animationDelay: `${delay}s`
         }
@@ -42,13 +59,14 @@ function Projects() {
     }
 
     const setPersonal = () => { 
-      let delay = 0.1 + (0.115 * projects.length)
-      return  personal.map( (project, i) => {
+      let delay = 0.1 + (0.115 * data.projects.length)
+      console.log(data)
+      return  data.personal.map( (project, i) => {
         let style = { 
           animationDelay: `${delay}s`
         }
         delay += 0.115
-        let index = projects.length + i;
+        let index = data.projects.length + i;
         return  <div className="tab slide-right" style={style}>
                   <input type="radio" id={`rd${index+1}`} name="rd"/>
                   <label className="tab-label tab-label-personal" data-text={project.name} htmlFor={`rd${index+1}`}>{project.name} </label>
@@ -81,11 +99,11 @@ function Projects() {
         <div className="accordion">
           <div className="section">from work</div>
           <div className="tabs">
-              {setContent()}
+              {data ? setContent() : ''}
           </div>
           <div className="section">personal</div>
           <div className="tabs">
-              {setPersonal()}
+              {data ? setPersonal() : ''}
           </div>
         </div>
       </div>
