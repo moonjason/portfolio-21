fcc5ae6e92974b90f8b0fe63d30f15626739281e
moonjason <jasonmoonjm@gmail.com>
2021-03-30 21:12:53 -0700

    update projects content, projects images, personal section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,5 @@
 import './projects.scss'
-import { projects } from './content.json';
+import { projects, personal } from './content.json';
 
 function Projects() {
 
@@ -10,23 +10,72 @@ function Projects() {
                   <label className="tab-label" data-text={project.name} htmlFor={`rd${i+1}`}>{project.name} </label>
                   <div className="tab-content">
                     <img className="tab-img" src={project.img} alt="project"/>
-                      {
-                        project.description.map(desc => {
-                          return <div className="tab-text">
-                                    {desc}
-                                  </div>
-                        })
-                      }
+                    <div className="tab-text">
+                      <br/>
+                       <span className="company">via {project.company}</span>
+                      <br/>
+                      <br/>
+                      {project.description.intro}
+                      <br/>
+                      <br/>
+                      {project.description.summary}
+                      <br/>
+                      <br/>
+                      <ul>
+                          {
+                            project.contributions.map((bullet, i) => <li className="task" key={`task-${i}`}>{bullet}</li>)
+                          }
+                      </ul>
+                      <br/>
+                      <br/>
+                      <p className="tech">Tech:</p>
+                      {project.tech}
+                    </div>
                   </div>
                 </div>
       })
     }
 
+    const setPersonal = () => { 
+      return  personal.map( (project, i) => {
+        let index = projects.length + i;
+        return  <div className="tab">
+                  <input type="radio" id={`rd${index+1}`} name="rd"/>
+                  <label className="tab-label tab-label-personal" data-text={project.name} htmlFor={`rd${index+1}`}>{project.name} </label>
+                  <div className="tab-content">
+                    <img className="tab-img" src={project.img} alt="project"/>
+                    <div className="tab-text">
+                      <br/>
+                      {project.description.intro}
+                      <br/>
+                      <br/>
+                      {project.description.summary}
+                      <br/>
+                      <br/>
+                      <ul>
+                          {
+                            project.contributions.map((bullet, i) => <li className="task" key={`task-${i}`}>{bullet}</li>)
+                          }
+                      </ul>
+                      <br/>
+                      <br/>
+                      <p className="tech">Tech:</p>
+                      {project.tech}
+                    </div>
+                  </div>
+                </div>
+      })
+    }
     return (
       <div className="container">
         <div className="accordion">
+          <div className="section">from work</div>
           <div className="tabs">
               {setContent()}
+          </div>
+          <div className="section">personal</div>
+          <div className="tabs">
+              {setPersonal()}
           </div>
         </div>
       </div>
